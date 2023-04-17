be8d8f9df2395aa8458f4f71a4dc765db52d69f9
Jorge Paul <jserranopaul@gmail.com>
2023-04-17 11:22:21 +0200

    Error handling

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ HTTP = Hyper Text Transfer Protocol
 import express from 'express';
 import ordersRouter from './routes/ordersRouter.js';
 import productsRouter from './routes/productsRouter.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 const port = process.env.PORT || 8000;
 const app = express();
 
 app.use('/products', productsRouter);
 app.use('/orders', ordersRouter);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,21 +1,12 @@
 import { Router } from 'express';
+import { createOrder, deleteOrder, getOrder, getOrders, updateOrder } from '../controllers/orders.js';
 
 const ordersRouter = Router();
 
-ordersRouter.get('/', (req, res) => {
-  res.json({ msg: 'GET all orders' });
-});
-ordersRouter.post('/', (req, res) => {
-  res.json({ msg: 'POST orders' });
-});
-ordersRouter.get('/:id', (req, res) => {
-  res.json({ msg: 'GET single orders' });
-});
-ordersRouter.put('/:id', (req, res) => {
-  res.json({ msg: 'PUT single orders' });
-});
-ordersRouter.delete('/:id', (req, res) => {
-  res.json({ msg: 'DEL single orders' });
-});
+ordersRouter.get('/', getOrders);
+ordersRouter.post('/', createOrder);
+ordersRouter.get('/:id', getOrder);
+ordersRouter.put('/:id', updateOrder);
+ordersRouter.delete('/:id', deleteOrder);
 
 export default ordersRouter;
